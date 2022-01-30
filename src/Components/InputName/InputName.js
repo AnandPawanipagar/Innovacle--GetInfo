d56f93639a35878ffe4ce97ffff73b788b0e3c85
AnandPawanipagar <85126312+AnandPawanipagar@users.noreply.github.com>
2022-01-30 16:37:08 +0530

    age, gender, natationality is stored and shown on the view

--- src/Components/InputName/InputName.js
+++ src/Components/InputName/InputName.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InputName.css";
 import { motion } from "framer-motion";
+import axios from "axios";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
 import { Grid } from "@mui/material";
 const InputName = () => {
+  const [inputVal, setInputVal] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState("");
+  const [nationality, setNationality] = useState([]);
+  const GetGender = () => {
+    axios.get(`https://api.genderize.io?name=${inputVal}`).then((r) => {
+      console.log(r.data);
+      setGender(r.data.gender);
+    });
+  };
+  const GetAge = () => {
+    axios.get(`https://api.agify.io?name=${inputVal}`).then((r) => {
+      console.log(r.data);
+      setAge(r.data.age);
+    });
+  };
+  const GetNationality = () => {
+    axios.get(`https://api.nationalize.io?name=${inputVal}`).then((r) => {
+      console.log(r.data);
+      setNationality(r.data.country);
+    });
+  };
   return (
     <>
       <Grid
@@ -24,19 +47,34 @@ const InputName = () => {
       >
         <Grid xs={12}>
           <TextField
+            onChange={(e) => {
+              setInputVal(e.target.value);
+            }}
             id="outlined-basic"
             label="Name"
             variant="outlined"
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
-                  <SearchIcon />
+                  <SearchIcon
+                    onClick={() => {
+                      GetGender();
+                      GetAge();
+                      GetNationality();
+                    }}
+                  />
                 </InputAdornment>
               ),
             }}
           />
         </Grid>
       </Grid>
+
+      {`name: ${inputVal} age:${age} nationality:${nationality
+        .map((obj) => {
+          return obj.country_id;
+        })
+        .join(",")}`}
     </>
   );
 };
